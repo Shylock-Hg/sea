d11068722d3f5c1f4f66e443190f1e9b6f23c2c3
Shylock Hg <tcath2s@gmail.com>
2019-09-23 21:19:29 +0800

    New question. (#14)

--- leetcode/greedy/src/main.rs
+++ leetcode/greedy/src/main.rs
@@ -1,6 +1,45 @@
 struct Solution {}
 
 impl Solution {
+    /// 0121. Best Time to Buy and Sell Stock
+    /// Say you have an array for which the ith element is the price of a given
+    /// stock on day i.
+    /// If you were only permitted to complete at most one transaction
+    /// (i.e., buy one and sell one share of the stock), design an algorithm to
+    /// find the maximum profit.
+    /// Note that you cannot sell a stock before you buy one.
+    pub fn max_profit(prices: Vec<i32>) -> i32 {
+        // Check all choose
+        // Time: O(N^2), Space: O(1)
+        // let mut profit = 0;
+        // for i in 0..prices.len() {
+        //     for j in i..prices.len() {
+        //         let now = prices[j] - prices[i];
+        //         if profit < now {
+        //             profit = now;
+        //         }
+        //     }
+        // }
+        // return profit;
+
+        // the possible maximum profit just based smaller buy price
+        if prices.len() == 0 {
+            return 0;
+        }
+        let mut min = prices.first().unwrap();
+        let mut profit = 0;
+        for i in 1..prices.len() {
+            // find the smaller price to buy
+            if min > &prices[i] {
+                min = &prices[i];
+            // compute the possible maximum profit
+            } else {
+                profit = std::cmp::max(profit, prices[i] - min);
+            }
+        }
+        return profit;
+    }
+
     /// 0435. Non-overlapping Intervals
     /// Given a collection of intervals, find the minimum number of intervals
     /// you need to remove to make the rest of the intervals non-overlapping.
@@ -126,6 +165,13 @@ fn main() {
 #[cfg(test)]
 mod testing {
     use super::Solution;
+
+    #[test]
+    fn test_max_profit() {
+        let Input = vec![7, 1, 5, 3, 6, 4];
+        let Output = 5;
+        assert_eq!(Output, Solution::max_profit(Input));
+    }
 
     #[test]
     fn test_erase_overlap_intervals() {
